55cf53d12dc770ac4fc799831e2fb36ce8975017
Mario Toledo <mariotoledo12@gmail.com>
2013-04-05 14:16:22 -0300

    Iniciando o projeto

---
+++ Models/NPartyDb.cs
@@ -0,0 +1,23 @@
+﻿using EixoX.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CampeonatosNParty.Models
+{
+    public class NPartyDb<T> : DatabaseStorage<T>
+    {
+        private static NPartyDb<T> _instance;
+        public static NPartyDb<T> Instance
+        {
+            get { return _instance ?? (_instance = new NPartyDb<T>()); }
+        }
+
+        private NPartyDb()
+            : base(new SqlServer(System.Configuration.ConfigurationManager.ConnectionStrings["NPartyCampeonatos"].ConnectionString), DatabaseAspect<T>.Instance)
+        {
+
+        }
+    }
+}
