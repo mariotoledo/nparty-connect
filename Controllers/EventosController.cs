dc51b7df807d903cff9af606993deda535e03dcd
Mario Toledo <mariotoledo12@gmail.com>
2013-04-17 10:26:27 -0300

    Criando Eventos Controller

---
+++ Controllers/EventosController.cs
@@ -0,0 +1,23 @@
+﻿using CampeonatosNParty.Models.Database;
+using CampeonatosNParty.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CampeonatosNParty.Controllers
+{
+    public class EventosController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
