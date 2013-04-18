ce11612c2211c486803c15ed244c623a643667ef
Mario Toledo <mariotoledo12@gmail.com>
2013-04-18 19:48:21 -0300

    Inserindo busca em Eventos

--- Controllers/EventosController.cs
+++ Controllers/EventosController.cs
@@ -1,5 +1,6 @@
 ﻿using CampeonatosNParty.Models.Database;
 using CampeonatosNParty.Models.ViewModel;
+using EixoX.Data;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -15,9 +16,20 @@ namespace CampeonatosNParty.Controllers
         //
         // GET: /Home/
 
+        ClassSelectResult<Eventos> result;
+
         public ActionResult Index()
         {
-            return View();
+            int page = 0;
+            int.TryParse(Request.QueryString["page"], out page);
+
+            ClassSelect<Eventos> search = Eventos.Search(Request.QueryString["filter"]);
+            search.Page(25, page);
+            search.OrderBy("Nome");
+
+            result = search.ToResult();
+
+            return View(result);
         }
     }
 }
