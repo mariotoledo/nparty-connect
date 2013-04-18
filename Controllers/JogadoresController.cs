88478ba4e2ef0665e92a74f108d81de86b8f898e
Mario Toledo <mariotoledo12@gmail.com>
2013-04-18 10:54:40 -0300

    Criando view de jogadores

---
+++ Controllers/JogadoresController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CampeonatosNParty.Controllers
+{
+    public class JogadoresController : Controller
+    {
+        //
+        // GET: /Jogadores/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Database/JogadoresItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EixoX.Restrictions;
+using EixoX.Data;
+using EixoX.Interceptors;
+using EixoX.UI;
+
+namespace CampeonatosNParty.Models.Database
+{
+    [DatabaseTable]
+    public class JogadoresItem : NPartyDbModel<JogadoresItem>
+    {
+        [DatabaseColumn]
+        public int IdUsuario { get; set; }
+
+        [DatabaseColumn]
+        public string NomeUsuario { get; set; }
+
+        [DatabaseColumn]
+        public string ApelidoUsuario { get; set; }
+
+        [DatabaseColumn]
+        public string Estado { get; set; }
+
+        [DatabaseColumn]
+        public int Pontos { get; set; }
+    }
+}
