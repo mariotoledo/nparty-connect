54e0c3400a1d832238e96788dbe9e8bd0f3c4cf9
Mario Toledo <mariotoledo12@gmail.com>
2013-04-18 10:23:11 -0300

    Criando pagina de view de Campeonatos

---
+++ Controllers/CampeonatosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CampeonatosNParty.Controllers
+{
+    public class CampeonatosController : Controller
+    {
+        //
+        // GET: /Campeonatos/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
