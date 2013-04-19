0fbdfc626a216865f690d4df3e254928b0653619
Mario Toledo <mariotoledo12@gmail.com>
2013-04-19 10:34:11 -0300

    Finalizando busca para view de Campeonatos

--- Controllers/CampeonatosController.cs
+++ Controllers/CampeonatosController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CampeonatosNParty.Models.Database;
+using EixoX.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,10 +13,20 @@ namespace CampeonatosNParty.Controllers
         //
         // GET: /Campeonatos/
 
+        ClassSelectResult<CampeonatosDashboardItem> result;
+
         public ActionResult Index()
         {
-            return View();
-        }
+            int page = 0;
+            int.TryParse(Request.QueryString["page"], out page);
 
+            ClassSelect<CampeonatosDashboardItem> search = CampeonatosDashboardItem.Search(Request.QueryString["filter"]);
+            search.Page(25, page);
+            search.OrderBy("DataCampeonato");
+
+            result = search.ToResult();
+
+            return View(result);
+        }
     }
 }
