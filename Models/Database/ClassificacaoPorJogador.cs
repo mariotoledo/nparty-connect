bd8d99cca08cb1d636f8dea0dd4f20c8c64ba97a
Mario Toledo <mariotoledo12@gmail.com>
2013-08-12 12:28:21 -0300

    Página de detalhes de jogadores

---
+++ Models/Database/ClassificacaoPorJogador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EixoX.Restrictions;
+using EixoX.Data;
+using EixoX.Interceptors;
+using EixoX.UI;
+
+namespace CampeonatosNParty.Models.Database
+{
+    [DatabaseTable]
+    public class ClassificacaoPorJogador : NPartyDbModel<ClassificacaoPorJogador>
+    {
+        [DatabaseColumn]
+        public int IdUsuario { get; set; }
+
+        [DatabaseColumn]
+        public int Pontuacao { get; set; }
+
+        [DatabaseColumn]
+        public DateTime DataCampeonato { get; set; }
+
+        [DatabaseColumn]
+        public string NomeCampeonato { get; set; }
+
+        [DatabaseColumn]
+        public string ImagemJogoCampeonato { get; set; }
+
+        [DatabaseColumn]
+        public string NomeEvento { get; set; }
+    }
+}
--- Models/ViewModel/UsuariosDetailView.cs
+++ Models/ViewModel/UsuariosDetailView.cs
@@ -1,4 +1,5 @@
 ﻿using CampeonatosNParty.Models.Database;
+using EixoX.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,15 @@ namespace CampeonatosNParty.Models.ViewModel
     public class UsuariosDetailView
     {
         public Usuarios usuario { get; set; }
+        public List<ClassificacaoPorJogador> classificacao { get; set; }
 
         public UsuariosDetailView(int userId)
         {
             usuario = Usuarios.WithIdentity(userId);
             if (usuario == null)
                 usuario = new Usuarios();
+
+            classificacao = ClassificacaoPorJogador.Select().Where("IdUsuario", userId).OrderBy("DataCampeonato", EixoX.Data.SortDirection.Descending).ToList();
         }
     }
 }
