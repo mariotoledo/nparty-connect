a92ebd7ae36e450c0265f94105c6d958877cac58
mariotoledo <mariotoledo12@gmail.com>
2013-08-23 15:40:33 -0300

    Finalizando tela de edição de usuário

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using CampeonatosNParty.Models.Database;
+using CampeonatosNParty.Models.StructModel;
 using EixoX.Data;
 using System;
 using System.Collections.Generic;
@@ -43,8 +44,59 @@ namespace CampeonatosNParty.Controllers
         [HttpGet]
         public ActionResult EditarUsuario(int? id)
         {
-            CampeonatosNParty.Models.Database.Usuarios view = NPartyDb<Usuarios>.Instance.WithIdentity(id.Value);
-            return View(view);
+            CampeonatosNParty.Models.Database.Usuarios user = NPartyDb<Usuarios>.Instance.WithIdentity(id.Value);
+            return View(new EditarInformacoes()
+            {
+                Apelido = user.Apelido,
+                Email = user.Email,
+                Id = user.Id,
+                Id_Cidade = user.Id_Cidade,
+                Id_Estado = user.Id_Estado, 
+                Nascimento = user.Nascimento, 
+                Nome = user.Nome, 
+                Telefone = user.Telefone
+            });
+        }
+
+        [AuthenticationRequired]
+        [HttpPost]
+        public ActionResult EditarUsuario(FormCollection form, EditarInformacoes model)
+        {
+            model.Nascimento = new DateTime(Int32.Parse(form["BirthdayYear"]), Int32.Parse(form["BirthdayMonth"]), Int32.Parse(form["BirthdayDay"]));
+
+            if (EixoX.Restrictions.RestrictionAspect<EditarInformacoes>.Instance.Validate(model))
+            {
+                if (string.IsNullOrEmpty(form["Id_Estado"]) || Int32.Parse(form["Id_Estado"]) == 0)
+                {
+                    ViewData["ErrorMessage"] = "Por favor, selecione o estado onde mora";
+                }
+                else if (string.IsNullOrEmpty(form["Id_Cidade"]) || Int32.Parse(form["Id_Cidade"]) == 0)
+                {
+                    ViewData["ErrorMessage"] = "Por favor, selecione a cidade onde mora";
+                }
+                else
+                {
+                    Usuarios u = CampeonatosNParty.Models.Database.Usuarios.WithIdentity(model.Id);
+                    if (u != null)
+                    {
+                        u.Apelido = model.Apelido;
+                        u.Email = model.Email;
+                        u.Id_Cidade = model.Id_Cidade;
+                        u.Id_Estado = model.Id_Estado;
+                        u.Nascimento = model.Nascimento;
+                        u.Nome = model.Nome;
+                        u.Telefone = model.Telefone;
+
+                        NPartyDb<Usuarios>.Instance.Save(u);
+                        ViewData["SuccessMessage"] = "Dados alterados com sucesso.";
+                    }
+                    else
+                    {
+                        ViewData["ErrorMessage"] = "Este e-mail já está registrado.";
+                    }
+                }
+            }
+            return View(model);
         }
 
     }
---
+++ Models/StructModel/EditarInformacoes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EixoX.Restrictions;
+using EixoX.Data;
+using EixoX.Interceptors;
+using EixoX.UI;
+
+namespace CampeonatosNParty.Models.StructModel
+{
+    public class EditarInformacoes
+    {
+        [DatabaseColumn(DatabaseColumnKind.Identity)]
+        [UIHidden]
+        public int Id { get; set; }
+
+        [DatabaseColumn]
+        [MaxLength(50)]
+        [Required]
+        [UISingleline]
+        public string Nome { get; set; }
+
+        [DatabaseColumn]
+        [MaxLength(50)]
+        [UISingleline]
+        public string Apelido { get; set; }
+
+        [DatabaseColumn]
+        [MaxLength(50)]
+        [Required]
+        [UISingleline]
+        public string Email { get; set; }
+
+        [DatabaseColumn]
+        [UISingleline]
+        [Required]
+        public DateTime Nascimento {get; set;}
+
+        [DatabaseColumn]
+        public int Id_Cidade { get; set; }
+
+        [DatabaseColumn]
+        public int Id_Estado { get; set; }
+
+        [DatabaseColumn]
+        [UISingleline]
+        public string Telefone { get; set; }
+    }
+}
