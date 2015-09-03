2d8e88e4b8b1f3281541d30f9b4c8f6fe824c5d3
Mario Toledo <mariotoledo12@gmail.com>
2015-09-03 19:23:48 -0300

    [www] adjusting home

---
+++ NParty.Www/NParty.Www/Controllers/NintendoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NParty.Www.Controllers
+{
+    public class NintendoController : Controller
+    {
+        //
+        // GET: /Nintendo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
