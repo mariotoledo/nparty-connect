e2fa380781df47533f1aa2ed6caad60d80792ca4
Mario Toledo <mariotoledo12@gmail.com>
2015-09-22 12:37:34 -0300

    Ajustes

---
+++ NParty.Www/NParty.Www/Controllers/ESportsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NParty.Www.Controllers
+{
+    public class ESportsController : Controller
+    {
+        //
+        // GET: /ESports/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
