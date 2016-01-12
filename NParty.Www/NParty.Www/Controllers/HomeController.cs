3c12cd0328bdadf20decd501a701c5c59b6188e0
Mario Toledo <mariotoledo12@gmail.com>
2016-01-12 09:58:52 -0200

    Página de sobre #24

--- NParty.Www/NParty.Www/Controllers/HomeController.cs
+++ NParty.Www/NParty.Www/Controllers/HomeController.cs
@@ -73,8 +73,6 @@ namespace NParty.Www.Controllers
             return View();
         }
 
-        public ActionResult Sobre() { return View(); }
-
         public JsonResult GetHilightItems()
         {
             NPartyArticlesHelper helper = new NPartyArticlesHelper();
---
+++ NParty.Www/NParty.Www/Controllers/SobreController.cs
@@ -0,0 +1,16 @@
+﻿using Google.GData.Client;
+using NParty.Www.Helpers;
+using NParty.Www.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NParty.Www.Controllers
+{
+    public class SobreController : NPartyController
+    {
+        public ActionResult QuemSomos() { return View(); }
+    }
+}
