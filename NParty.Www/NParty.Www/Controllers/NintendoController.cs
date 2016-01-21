f80bdd49d90dc225b7dc0766a3ca2087b01ca320
Mario Toledo <mariotoledo12@gmail.com>
2016-01-21 19:55:48 -0200

    ajustes

--- NParty.Www/NParty.Www/Controllers/NintendoController.cs
+++ NParty.Www/NParty.Www/Controllers/NintendoController.cs
@@ -36,7 +36,10 @@ namespace NParty.Www.Controllers
         public ActionResult Top10(int? page) { return OpenPageByLabel("Top 10", page); }
         public ActionResult Mario(int? page) { return OpenPageByLabel("Mario", page); }
         public ActionResult Pokemon(int? page){ return OpenPageByLabel("Pokémon", page); }
-        public ActionResult Pokemon20Anos(int? page) { return OpenPageByLabel("Pokémon", page); }
+        public ActionResult Pokemon20Anos(int? page) {
+            ViewData["DailyMusicVideoId"] = GetDailyPokemonMusic()[DateTime.Now.Day];
+            return OpenPageByLabel("Pokémon", page);
+        }
         public ActionResult TheLegendOfZelda(int? page){ return OpenPageByLabel("The Legend of Zelda", page); }
 
         private ActionResult OpenPageByLabel(string label, int? page)
@@ -83,6 +86,45 @@ namespace NParty.Www.Controllers
         {
             ArticlesHelper helper = new ArticlesHelper();
             return Json(helper.GetArticlesFromBlog(NintendoBlogId, "Nintendo", 3, 0, label), JsonRequestBehavior.AllowGet);
+        }
+
+        private Dictionary<int, string>  GetDailyPokemonMusic()
+        {
+            Dictionary<int, string> youtubeVideoIdByDay = new Dictionary<int, string>();
+
+            youtubeVideoIdByDay.Add(1, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(2, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(3, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(4, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(5, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(6, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(7, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(8, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(9, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(10, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(11, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(12, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(13, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(14, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(15, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(16, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(17, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(18, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(19, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(20, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(21, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(22, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(23, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(24, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(25, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(26, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(27, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(28, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(29, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(30, "vXJgQLknzfI");
+            youtubeVideoIdByDay.Add(31, "vXJgQLknzfI");
+
+            return youtubeVideoIdByDay;
         }
     }
 }
