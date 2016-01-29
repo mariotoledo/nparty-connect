5dbc7d76d176f70229469a969ec1f1dc154baa2e
Mario Toledo <mariotoledo12@gmail.com>
2016-01-29 14:22:12 -0200

    ajustes

--- NParty.Www/NParty.Www/Content/pokemontimeline/rpgjs/core/scene/Scene_Window.js
+++ NParty.Www/NParty.Www/Content/pokemontimeline/rpgjs/core/scene/Scene_Window.js
@@ -18,7 +18,7 @@ RPGJS_Canvas.Scene.New({
 	
 		var self = this;
 		
-		this.window = RPGJS_Canvas.Window.New(this, 500, 200, "window");
+		this.window = RPGJS_Canvas.Window.New(this, 500, 130, "window");
 		this.window.setBackground("white", 6, 1);
 		
 		this.window.position("bottom");
@@ -54,7 +54,7 @@ RPGJS_Canvas.Scene.New({
 			height = array.length * 35 + 25;
 			
 		var box = RPGJS_Canvas.Window.New(this, width, height, "window");
-		box.setBackground("white", 6, .7);
+		box.setBackground("white", 6, 1);
 		// var box = RPGJS_Canvas.Window.new(this, width, height);
 			
 		box.position("top");
@@ -66,7 +66,7 @@ RPGJS_Canvas.Scene.New({
 			text = RPGJS_Canvas.Text.New(this, array[i]);
 			text.style({
 				size: "12px",
-				color: "white",
+				color: "black",
 				family: 'Pokemon'
 			}).draw(el, 0, 10);
 			array_el.push(el);
@@ -75,9 +75,9 @@ RPGJS_Canvas.Scene.New({
 
 	
 		var cursor = this.createElement();
-		cursor.fillStyle = "white";
+		cursor.fillStyle = "blue";
 		cursor.fillRect(-10, -10, width-30, 30);
-		cursor.opacity = 1;
+		cursor.opacity = 0.3;
 
 		box.cursor.init(cursor, array_el, {
 			enter: [Input.Enter, Input.Space]
