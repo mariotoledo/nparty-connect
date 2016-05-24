cbc6c7f0a0915de7a211f58ab5bd870bc91b17ff
Mario Toledo <mariotoledo12@gmail.com>
2016-05-23 22:14:26 -0300

    User

---
+++ NParty.Www/NParty.Www/DatabaseModel/User.cs
@@ -0,0 +1,48 @@
+﻿using EixoX.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NParty.Www.DatabaseModel
+{
+    [DatabaseTable]
+    public class User
+    {
+        [DatabaseColumn(DatabaseColumnKind.Identity)]
+        public int UserId { get; set; }
+
+        [DatabaseColumn]
+        public string Username { get; set; }
+
+        [DatabaseColumn]
+        public string FirstName { get; set; }
+
+        [DatabaseColumn]
+        public string LastName { get; set; }
+
+        [DatabaseColumn]
+        public string Email { get; set; }
+
+        [DatabaseColumn]
+        public bool IsValidEmail { get; set; }
+
+        [DatabaseColumn]
+        public string PasswordHash { get; set; }
+
+        [DatabaseColumn(true)]
+        public DateTime Birthdate { get; set; }
+
+        [DatabaseColumn]
+        public int StateId { get; set; }
+
+        [DatabaseColumn]
+        public int CityId { get; set; }
+
+        [DatabaseColumn]
+        public bool NewsletterOption { get; set; }
+
+        [DatabaseColumn]
+        public string PhotoUrl { get; set; }
+    }
+}
