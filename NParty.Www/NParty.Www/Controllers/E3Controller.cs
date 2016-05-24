d7f4a39ecd73136c686203250f04576e32f1b067
Mario Toledo <mariotoledo12@gmail.com>
2016-05-24 09:20:47 -0300

    ajustes

---
+++ NParty.Www/NParty.Www/Controllers/E3Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NParty.Www.Controllers
+{
+    public class E3Controller : Controller
+    {
+        //
+        // GET: /E3/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
