759a48066af737b208ad5d4aa6c439cd21c090ac
Mario Toledo <mariotoledo12@gmail.com>
2016-05-31 02:25:57 -0300

    E3 ajustes

--- NParty.Www/NParty.Www/Controllers/E3Controller.cs
+++ NParty.Www/NParty.Www/Controllers/E3Controller.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Google.Apis.Blogger.v3;
+using NParty.Www.Helpers;
+using NParty.Www.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,13 +9,33 @@ using System.Web.Mvc;
 
 namespace NParty.Www.Controllers
 {
-    public class E3Controller : Controller
+    public class E3Controller : NPartyController
     {
         //
         // GET: /E3/
 
         public ActionResult Index()
         {
+            string apiKey = System.Configuration.ConfigurationManager.AppSettings["PodcastBloggerApiKey"];
+            string appName = System.Configuration.ConfigurationManager.AppSettings["PodcastGoogleAppName"];
+
+            BloggerService service = new BloggerService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                ApiKey = apiKey,
+                ApplicationName  = appName
+            });
+
+            ArticlesHelper helper = new ArticlesHelper(apiKey, appName, service);
+
+            List<Article> nintendoa3Articles = helper.GetArticlesFromBlog(NintendoBlogId, "Nintendo", 100, 1, "E3", "/Artigos/Ler/");
+            List<Article> costaACostaArticles = helper.GetArticlesFromBlog(MainBlogId, "Home", 100, 1, "E3", "/NPartyCostaACosta/Ouvir/");
+
+            List<Article> podcasts = new List<Article>();
+            podcasts.AddRange(nintendoa3Articles);
+            podcasts.AddRange(costaACostaArticles);
+
+            ViewData["articles"] = podcasts.OrderByDescending(t => t.DatePublished).ToList();
+
             return View();
         }
 
