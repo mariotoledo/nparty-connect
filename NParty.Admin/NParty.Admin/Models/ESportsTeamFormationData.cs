e0f33b1689abeb630a692c671449e480ace58cea
Mario Toledo <mariotoledo12@gmail.com>
2016-07-14 18:49:56 -0300

    starting Team Formation

---
+++ NParty.Admin/NParty.Admin/Models/ESportsTeamFormationData.cs
@@ -0,0 +1,15 @@
+﻿using NParty.Database.ESports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NParty.Admin.Models
+{
+    public class ESportsTeamFormationData
+    {
+        public ESportsTeam ESportsTeam { get; set; }
+
+        public List<ESportsGameRule> ESportsGameRules { get; set; }
+    }
+}
--- NParty.Database/NParty.Database/ESports/ESportsGamePlayerPositionDetail.cs
+++ NParty.Database/NParty.Database/ESports/ESportsGamePlayerPositionDetail.cs
@@ -8,15 +8,30 @@ using System.Threading.Tasks;
 namespace NParty.Database.ESports
 {
     [DatabaseTable]
-    public class ESportsTeamFormationPlayer : ESportsDbModel<ESportsTeamFormationPlayer>
+    public class ESportsGamePlayerPositionDetail
     {
         [DatabaseColumn]
         public int ESportsTeamFormationId { get; set; }
 
         [DatabaseColumn]
+        public int ESportsGameId { get; set; }
+
+        [DatabaseColumn]
+        public DateTime DateFormation { get; set; }
+
+        [DatabaseColumn]
         public int ESportsPlayerId { get; set; }
 
         [DatabaseColumn]
+        public string ESportsPlayerNickname { get; set; }
+
+        [DatabaseColumn]
         public int ESportsGamePlayerPositionId { get; set; }
+
+        [DatabaseColumn]
+        public string ESportsGamePositionName { get; set; }
+
+        [DatabaseColumn]
+        public bool IsRequired { get; set; }
     }
 }
